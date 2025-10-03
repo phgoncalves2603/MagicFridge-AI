882612c518246c0c465b302ddab54652d76be18a
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-02 23:53:03 -0400

    feat: add gemini Service

--- src/main/java/dev/java/demo/controller/RecipeControler.java
+++ src/main/java/dev/java/demo/controller/RecipeControler.java
@@ -1,6 +1,7 @@
 package dev.java.demo.controller;
 
 import dev.java.demo.service.ChatGptService;
+import dev.java.demo.service.GeminiService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +10,25 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class RecipeControler {
-    ChatGptService chatGptService;
+    //ChatGptService chatGptService;
+    GeminiService geminiService;
 
-    public RecipeControler(ChatGptService chatGptService){
-        this.chatGptService = chatGptService;
+    public RecipeControler(ChatGptService chatGptService, GeminiService geminiService){
+        //this.chatGptService = chatGptService;
+        this.geminiService = geminiService;
     }
-    @GetMapping("/generate")
-    public Mono<ResponseEntity<String>> generateRecipe(){
-        return chatGptService.generateRecipe()
+//    @GetMapping("/generate")
+//    public Mono<ResponseEntity<String>> generateRecipe(){
+//        return chatGptService.generateRecipe()
+//                .map(recipe -> ResponseEntity.ok(recipe))
+//                .defaultIfEmpty(ResponseEntity.noContent().build());
+//    }
+    @GetMapping("/gemini")
+    public Mono<ResponseEntity<String>> generateRecipeGemini(){
+        return geminiService.generateRecipe()
                 .map(recipe -> ResponseEntity.ok(recipe))
                 .defaultIfEmpty(ResponseEntity.noContent().build());
     }
-
-
-
 }
 
 
